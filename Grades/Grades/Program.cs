8f0c292b93034ceeccc34628806702fc39ee66e8
Masood_Alam <mohamed.masood.alam@philips.com>
2016-11-06 15:25:45 +0530

    Adding First Version- with Array and main method

---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] grades = new float[] {66.0f,78.5f,89.1f,98.0f};
+            float sum = 0f;
+            float lowest = 100f;
+            float highest = 0f;
+            foreach (float grade in grades) {
+                sum = sum + grade;
+                if (grade < lowest)
+                {
+                    lowest = grade;
+                }
+                if (grade > highest)
+                {
+                    highest = grade;
+                }
+            }
+            float average = sum / grades.Length;
+            Console.WriteLine("Lowest grade is {0}",lowest);
+            Console.WriteLine("Highest grade is {0}",highest);
+            Console.WriteLine("Average of grades is {0}",average);
+
+            Console.ReadLine();
+        }
+    }
+}
